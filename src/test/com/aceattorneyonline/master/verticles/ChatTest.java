0b2fecb80ba3ff6b0f66a1b4b096336375239442
oldmud0 <oldmud0@users.noreply.github.com>
2017-08-05 17:25:41 -0500

    Fix package typo in test directory

--- src/test/com/aceattorneyonline/master/verticles/ChatTest.java
+++ src/test/com/aceattorneyonline/master/verticles/ChatTest.java
@@ -1,4 +1,4 @@
-package com.aceatorneyonline.master.verticles;
+package com.aceattorneyonline.master.verticles;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
