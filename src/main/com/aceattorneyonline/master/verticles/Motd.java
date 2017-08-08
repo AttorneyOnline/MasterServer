f665b9a805caed40a41fb28b5610434aa238168e
oldmud0 <oldmud0@users.noreply.github.com>
2017-08-07 20:36:59 -0500

    Implement set/reloadMotd with preprocessor

--- src/main/com/aceattorneyonline/master/verticles/Motd.java
+++ src/main/com/aceattorneyonline/master/verticles/Motd.java
@@ -1,18 +1,30 @@
 package com.aceattorneyonline.master.verticles;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Formatter;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.UUID;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.aceattorneyonline.master.ChatCommandSyntax;
 import com.aceattorneyonline.master.MasterServer;
+import com.aceattorneyonline.master.Player;
 import com.aceattorneyonline.master.events.AdminEventProtos.ReloadMotd;
 import com.aceattorneyonline.master.events.AdminEventProtos.SetMotd;
 import com.aceattorneyonline.master.events.EventErrorReason;
 import com.aceattorneyonline.master.events.Events;
 import com.aceattorneyonline.master.events.SharedEventProtos.GetMotd;
 import com.aceattorneyonline.master.events.UuidProto.Uuid;
+import com.google.protobuf.InvalidProtocolBufferException;
 
 import io.vertx.core.eventbus.EventBus;
 import io.vertx.core.eventbus.Message;
@@ -20,7 +32,9 @@ import io.vertx.core.eventbus.Message;
 public class Motd extends ClientListVerticle {
 
 	private static final Logger logger = LoggerFactory.getLogger(Motd.class);
-	
+
+	private static final String MOTD_FILE_NAME = "motd.txt";
+
 	private String motd = "Welcome to Attorney Online! Master Server version: " + MasterServer.VERSION;
 
 	@Override
@@ -31,6 +45,13 @@ public class Motd extends ClientListVerticle {
 		eventBus.consumer(Events.GET_MOTD.getEventName(), this::handleGetMotd);
 		eventBus.consumer(Events.SET_MOTD.getEventName(), this::handleSetMotd);
 		eventBus.consumer(Events.RELOAD_MOTD.getEventName(), this::handleReloadMotd);
+
+		try {
+			motd = getMotdFile(MOTD_FILE_NAME);
+			logger.debug("Loaded MOTD from {}", MOTD_FILE_NAME);
+		} catch (IOException e) {
+			logger.error("Failed to load MOTD!", e);
+		}
 	}
 
 	@Override
@@ -39,11 +60,30 @@ public class Motd extends ClientListVerticle {
 	}
 
 	public void handleSetMotd(Message<byte[]> event) {
-		event.fail(EventErrorReason.INTERNAL_ERROR, "not implemented"); // FIXME handleSetMotd
+		try {
+			SetMotd setMotd = SetMotd.parseFrom(event.body());
+			UUID id = UUID.fromString(setMotd.getId().getId());
+			String newMessage = setMotd.getMessage();
+			Player requestingPlayer = getPlayerById(id);
+
+			if (requestingPlayer == null) {
+				event.fail(EventErrorReason.SECURITY_ERROR, "Requester is not a player.");
+				return;
+			} else if (!requestingPlayer.hasAdmin()) {
+				event.fail(EventErrorReason.SECURITY_ERROR, "Requester does not have admin rights.");
+				return;
+			}
+
+			motd = newMessage;
+			event.reply("Success.");
+		} catch (InvalidProtocolBufferException e) {
+			event.fail(EventErrorReason.INTERNAL_ERROR, "Could not parse SetMotd protobuf");
+		}
 	}
-	
+
 	@ChatCommandSyntax(name = "setmotd", description = "Sets the MOTD permanently.", arguments = "<message>")
-	public static com.google.protobuf.Message parseSetMotd(Uuid invoker, List<String> args) throws IllegalArgumentException {
+	public static com.google.protobuf.Message parseSetMotd(Uuid invoker, List<String> args)
+			throws IllegalArgumentException {
 		if (args.isEmpty()) {
 			throw new IllegalArgumentException();
 		}
@@ -52,22 +92,99 @@ public class Motd extends ClientListVerticle {
 	}
 
 	public void handleReloadMotd(Message<byte[]> event) {
-		event.fail(EventErrorReason.INTERNAL_ERROR, "not implemented"); // FIXME handleReloadMotd
+		try {
+			ReloadMotd reload = ReloadMotd.parseFrom(event.body());
+			UUID id = UUID.fromString(reload.getId().getId());
+			Player requestingPlayer = getPlayerById(id);
+
+			if (requestingPlayer == null) {
+				event.fail(EventErrorReason.SECURITY_ERROR, "Requester is not a player.");
+				return;
+			} else if (!requestingPlayer.hasAdmin()) {
+				event.fail(EventErrorReason.SECURITY_ERROR, "Requester does not have admin rights.");
+				return;
+			}
+
+			motd = getMotdFile(MOTD_FILE_NAME);
+			event.reply("Success.");
+		} catch (InvalidProtocolBufferException e) {
+			event.fail(EventErrorReason.INTERNAL_ERROR, "Could not parse ReloadMotd protobuf");
+		} catch (IOException e) {
+			event.fail(EventErrorReason.INTERNAL_ERROR, e.getMessage());
+		}
 	}
-	
+
 	@ChatCommandSyntax(name = "reloadmotd", description = "Reloads the MOTD to file.", arguments = "")
-	public static com.google.protobuf.Message parseReloadMotd(Uuid invoker, List<String> args) throws IllegalArgumentException {
+	public static com.google.protobuf.Message parseReloadMotd(Uuid invoker, List<String> args)
+			throws IllegalArgumentException {
 		return ReloadMotd.newBuilder().setId(invoker).build();
 	}
-	
+
 	public void handleGetMotd(Message<byte[]> event) {
-		event.reply(motd);
+		event.reply(Preprocessor.preprocess(motd));
 	}
-	
+
 	@ChatCommandSyntax(name = "motd", description = "Gets the message of the day.", arguments = "")
-	public static com.google.protobuf.Message parseGetMotd(Uuid invoker, List<String> args) throws IllegalArgumentException {
+	public static com.google.protobuf.Message parseGetMotd(Uuid invoker, List<String> args)
+			throws IllegalArgumentException {
 		return GetMotd.newBuilder().build();
 	}
-	
+
+	private String getMotdFile(String filename) throws IOException {
+		try {
+			logger.debug("Reading welcome file {}", filename);
+			return new String(Files.readAllBytes(new File(filename).toPath()), "UTF-8");
+		} catch (IOException e) {
+			IOException detailedE = new IOException("Could not load MOTD file " + filename + "!", e);
+			logger.error(detailedE.getMessage(), detailedE);
+			throw detailedE;
+		}
+	}
+
+	private static class Preprocessor {
+		private static final Map<String, Macro> macros = new HashMap<>();
+		static {
+			macros.put("version", () -> MasterServer.VERSION);
+			macros.put("uptime", () -> {
+				Duration uptime = Duration.between(MasterServer.START_TIME, Instant.now());
+				long days = uptime.toDays(), hours = uptime.toHours() % 24, minutes = uptime.toMinutes() % 60,
+						totalSeconds = uptime.getSeconds();
+				Formatter formatter = new Formatter();
+				if (totalSeconds < 60)
+					// Don't print seconds except if the master server has only been up for a very
+					// short amount of time.
+					formatter.format("%d seconds", totalSeconds);
+				else {
+					if (days > 0)
+						formatter.format("%d day%s", days, days > 1 ? "s" : "");
+					if (hours > 0)
+						formatter.format("%d hour%s", hours, hours > 1 ? "s" : "");
+					if (minutes > 0)
+						formatter.format("%d minute%s", minutes, minutes > 1 ? "s" : "");
+				}
+				String out = formatter.toString();
+				formatter.close();
+				return out;
+			});
+		}
+
+		/**
+		 * Preprocesses a MOTD. Supported macros:
+		 * <ul>
+		 * <li><code>${version}</code></li>
+		 * <li><code>${uptime}</code></li>
+		 * </ul>
+		 */
+		public static String preprocess(String motd) {
+			for (Entry<String, Macro> entry : macros.entrySet()) {
+				motd = motd.replaceAll("\\$\\{" + entry.getKey() + "\\}", entry.getValue().expand());	
+			}
+			return motd;
+		}
+
+		interface Macro {
+			String expand();
+		}
+	}
 
 }
