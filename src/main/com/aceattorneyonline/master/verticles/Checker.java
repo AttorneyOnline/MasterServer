04f65f8ada2e72cb78524295f54da001b8566656
oldmud0 <oldmud0@users.noreply.github.com>
2017-12-09 14:57:26 -0600

    Periodic checker: put a lock on client list

--- src/main/com/aceattorneyonline/master/verticles/Checker.java
+++ src/main/com/aceattorneyonline/master/verticles/Checker.java
@@ -1,5 +1,7 @@
 package com.aceattorneyonline.master.verticles;
 
+import java.util.Collection;
+
 import com.aceattorneyonline.master.Client;
 
 import io.vertx.core.AbstractVerticle;
@@ -8,9 +10,14 @@ public class Checker extends AbstractVerticle {
 	private long checkTimer;
 
 	public void start() {
-		checkTimer = vertx.setPeriodic(3500, id -> {
-			for (Client client : ClientServerList.getSingleton().getClientsList()) {
-				client.protocolWriter().sendConnectionCheck();
+		checkTimer = vertx.setPeriodic(5000, id -> {
+			Collection<Client> clients = ClientServerList.getSingleton().getClientsList();
+			// Don't allow other threads to modify client list! Otherwise a
+			// ConcurrentModificationException may occur.
+			synchronized (clients) {
+				for (Client client : clients) {
+					client.protocolWriter().sendConnectionCheck();
+				}
 			}
 		});
 	}
